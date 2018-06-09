63551241fa57852da341b9d0bafe96603266c667
RJ_Minal <rj.minera@gmail.com>
2018-06-09 12:12:31 -0600

    add: clearing cart function after placing order

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { Order } from './../models/order';
+import { AuthService } from './../services/auth.service';
 import { OrderService } from './../services/order.service';
 import { Subscription } from 'rxjs/Subscription';
 import { ShoppingCartService } from './../services/shopping-cart.service';
@@ -12,40 +15,34 @@ import { ShoppingCart } from '../models/shopping-cart';
 export class CheckOutComponent implements OnInit, OnDestroy { 
   shipping = {};
   cart: ShoppingCart;
-  subscription: Subscription;
+  userId: string;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
 
   constructor(
+    private router: Router,
+    private authService: AuthService,
     private cartService: ShoppingCartService,
     private orderService: OrderService
   ) {}
 
   async ngOnInit(){
     let cart$ = await this.cartService.getCart();
-    this.subscription = cart$.subscribe(cart => this.cart = cart);
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.userSubscription = this.authService.user$
+      .subscribe(user => this.userId = user.uid);
   }
 
   ngOnDestroy(){
-    this.subscription.unsubscribe();
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
   
-  placeOrder() {
-    let order = {
-      datePlaced: new Date().getTime(),
-      shipping: this.shipping,
-      items: this.cart.items.map(i => {
-        return {
-          product: {
-            title: i.title,
-            imageUrl: i.imageUrl,
-            price: i.price
-          },
-          quantity: i.quantity,
-          totalPrice: i.totalPrice
-        }
-      })
-    }
-
-    this.orderService.storeOrder(order);
+  async placeOrder() {
+    let order = new Order(this.userId,this.shipping,this.cart);
+    let result = await this.orderService.placeOrder(order);
+    
+    this.router.navigate(['/order-success', result.key]);
   }
 
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,13 +1,19 @@
+import { ShoppingCartService } from './shopping-cart.service';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(
+    private db: AngularFireDatabase,
+    private cartService: ShoppingCartService
+  ) {}
 
-  storeOrder(order){
-    return this.db.list('/orders').push(order);
+  async placeOrder(order){
+    let result = await this.db.list('/orders').push(order);
+    this.cartService.clearCart();
+    return result;
   }
 
 }
